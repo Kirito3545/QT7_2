81a7664938b2a4f84df40e58fbb4718b88a27a58
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-11-02 16:58:37 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <QSqlDatabase>
 #include <QTcpSocket>
 
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL7");
+    QTcpSocket* socket;
 
     return a.exec();
 }
